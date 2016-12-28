4ed5887cc4620404c332402692f78d8ed1f3f229
caasiu <Rogers619952467@gmail.com>
2016-12-28 21:53:26 +0000

    use HTTP cache to avoid researching database

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const crypto = require('crypto');
 
 app.use('/', express.static(path.join(__dirname, 'src')));
 //app.use((req, res) => res.sendFile(`${__dirname}/src/index.html`));
@@ -23,16 +24,32 @@ const price2float = (rows) => {
     return result;
 }
 
+const generateHash = (data) => {
+    return crypto.createHash('md5')
+        .update(data)
+        .digest('hex');
+}
+
+var productETag;
 app.get('/api/product/:slug', (req, res) => {
-    var client = new pg.Client(config);
-    client.connect();
-    client.query("select * from products where slug = $1", [req.params.slug])
-        .then(result => {
-            res.json(result.rows);
-        }).then(() => {
-            client.end();
-            console.log('fetch api/product/'+ req.params.slug +' finished');
-        });
+    if (!productETag || req.headers["if-none-match"] !== productETag){
+        var client = new pg.Client(config);
+        client.connect();
+        client.query("select * from products where slug = $1", [req.params.slug])
+            .then(result => {
+                productETag = generateHash(Buffer.from(result.rows));
+                res.set({"ETag": productETag});
+                res.json(result.rows);
+            }).then(() => {
+                client.end();
+                console.log('fetch api/product/'+ req.params.slug +' finished');
+            });
+    }else{
+        // Here is using HTTP Cache to avoid refetching database
+        console.log("product data is using browser cache")
+        res.set({"ETag": productETag});
+        res.send();
+    }
 });
 
 app.get('/api/id/:num', (req, res) => {
@@ -47,16 +64,26 @@ app.get('/api/id/:num', (req, res) => {
         });
 })
 
+var rowsETag;
 app.get('/api/products', (req, res) => {
-    var client = new pg.Client(config);
-    client.connect();
-    client.query("select id, label, image, slug, introduction, price, stock from products")
-        .then(result => {
-            res.json(price2float(result.rows));
-        }).then(() => {
-            client.end();
-            console.log('fetch all products finished');
-        });
+    if (!rowsETag || req.headers["if-none-match"] !== rowsETag){
+        var client = new pg.Client(config);
+        client.connect();
+        client.query("select id, label, image, slug, introduction, price, stock from products")
+            .then(result => {
+                rowsETag = generateHash(Buffer.from(result.rows));
+                res.set({"ETag": rowsETag});
+                res.json(price2float(result.rows));
+            }).then(() => {
+                client.end();
+                console.log('fetch all products finished');
+            });
+    } else {
+        // Here is using HTTP Cache to avoid refetching database
+        console.log("products list is using browser cache")
+        res.set({"ETag": rowsETag});
+        res.send();
+    }
 })
 
 app.listen(3000, () => console.log('express port is on: 3000'));
